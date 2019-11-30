28fc73d263ffdab83c93b1b2d9aaba538dbdf775
hamza bhatti <hamzagulzar876@gmail.com>
2019-12-01 02:00:27 +0500

    Add project files.

---
+++ TryGitHub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TryGitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
